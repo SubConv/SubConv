ef8d66f16d31f4b1c675f9b317a60d801a76d065
PKC278 <pengkaichen@foxmail.com>
2024-06-14 19:55:48 +0800

    feat: Added apple-touch-icon link to index.html and optimized styling in App.vue

--- mainpage/src/App.vue
+++ mainpage/src/App.vue
@@ -1,43 +1,53 @@
 <template>
-    <div>
+    <div class="container">
         <el-card class="box-card">
             <div class="header">
-                <a href="https://github.com/SubConv/SubConv">
-                <span>
-                    SubConv
-                </span>
-                <span>
-                    <i class="fa-brands fa-github"></i>
-                </span>
+                <a href="https://github.com/SubConv/SubConv" target="_blank">
+                    <span>
+                        SubConv
+                    </span>
+                    <span>
+                        <i class="fa-brands fa-github"></i>
+                    </span>
                 </a>
+                <el-button type="danger" @click="resetForm">重置</el-button>
             </div>
 
-            <el-form label-position="right" label-width="100px" class="main">
-                <el-form-item label="订阅">
-                    <el-input type="textarea" v-model="linkInput" rows="5" resize="none"
+            <el-form label-position="top" label-width="100px" class="main">
+                <el-form-item label="订阅链接">
+                    <el-input type="textarea" v-model="form.linkInput" rows="5" resize="none"
                         placeholder="请粘贴订阅链接，或者分享链接，多个订阅链接请换行或用|符号隔开"></el-input>
                 </el-form-item>
 
-                <el-form-item label="代理规则集">
-                    <el-switch v-model="proxy_switch" active-text="关闭后将直接从GitHub获取规则集而非通过本服务器代理"></el-switch>
+                <el-form-item label="高级设置">
+                    <el-switch v-model="form.advanced_switch" active-text="开启后可以查看更多设置"></el-switch>
                 </el-form-item>
 
-                <el-form-item label="备用节点">
-                    <el-switch v-model="standby_switch" active-text="备用节点只会出现在手动选择分组"></el-switch>
-                    <el-input type="textarea" v-model="standby" rows="5" resize="none" v-if="standby_switch"
-                        placeholder="请粘贴备用节点，多个备用节点请换行或用|符号隔开"></el-input>
-                </el-form-item>
+                <template v-if="form.advanced_switch">
+                    <el-form-item label="代理规则集">
+                        <el-switch v-model="form.proxy_switch" active-text="关闭后将直接从GitHub获取规则集而非通过本服务器代理"></el-switch>
+                    </el-form-item>
 
-                <el-form-item label="更新间隔">
-                    <el-input v-model="time" style="width: 100px" placeholder=""></el-input>
-                    秒，默认为1800
-                </el-form-item>
+                    <el-form-item label="备用节点">
+                        <el-switch v-model="form.standby_switch" active-text="备用节点只会出现在手动选择分组"></el-switch>
+                        <el-input type="textarea" v-model="form.standby" rows="5" resize="none"
+                            v-if="form.standby_switch" placeholder="请粘贴备用节点，多个备用节点请换行或用|符号隔开"></el-input>
+                    </el-form-item>
+
+                    <el-form-item label="更新间隔">
+                        <el-input v-model="form.time" style="width: 100px" placeholder="1800"></el-input>
+                        &nbsp;秒
+                    </el-form-item>
+                </template>
+
                 <el-form-item label="新订阅链接">
-                    <el-input type="textarea" v-model="linkOutput" rows="2" resize="none"></el-input>
+                    <el-input type="textarea" v-model="form.linkOutput" rows="2" resize="none" readonly></el-input>
                 </el-form-item>
                 <el-form-item>
                     <el-button type="primary" @click="submitForm">生成</el-button>
                     <el-button @click="copyForm">复制</el-button>
+                    <el-button :disabled="!form.linkOutput" @click="installClash"
+                        class="clash-button">导入Clash</el-button>
                 </el-form-item>
             </el-form>
         </el-card>
@@ -67,100 +77,183 @@
 </template>
 
 <script setup lang="ts">
-// init
-import { ref } from 'vue'
-import { ElButton, ElInput, ElForm, ElFormItem, ElCard, ElSwitch, ElMessage } from 'element-plus'
-import 'element-plus/es/components/button/style/css'
-import 'element-plus/es/components/input/style/css'
-import 'element-plus/es/components/form/style/css'
-import 'element-plus/es/components/form-item/style/css'
-import 'element-plus/es/components/card/style/css'
-import 'element-plus/es/components/switch/style/css'
-import 'element-plus/es/components/message/style/css'
-const linkInput = ref('')
-const linkOutput = ref('')
-const time = ref('')
-const standby = ref('')
-const standby_switch = ref(false)
-const proxy_switch = ref(true)
+    // init
+    import { ref, onMounted } from 'vue'
+    import { ElButton, ElInput, ElForm, ElFormItem, ElCard, ElSwitch, ElMessage } from 'element-plus'
+    import 'element-plus/es/components/button/style/css'
+    import 'element-plus/es/components/input/style/css'
+    import 'element-plus/es/components/form/style/css'
+    import 'element-plus/es/components/form-item/style/css'
+    import 'element-plus/es/components/card/style/css'
+    import 'element-plus/es/components/switch/style/css'
+    import 'element-plus/es/components/message/style/css'
 
-// methods
-const submitForm = () => {
-    let result = window.location.protocol + "//" + window.location.host
-    if (linkInput.value !== "") {
-        result += "/sub?url=" + encodeURIComponent(linkInput.value);
-        if (time.value !== "") {
-            if (/^[1-9][0-9]*$/.test(time.value)) {
-                result += "&interval=" + time.value;
-            }
-            else {
-                ElMessage({
-                    message: '时间间隔必须为整数',
-                    type: 'error'
-                });
-                linkOutput.value = ""
-                return false;
-            }
-        }
-        if (standby_switch.value) {
-            if (standby.value !== "") {
-                result += "&urlstandby=" + encodeURIComponent(standby.value);
-            }
-        }
-        if (!proxy_switch.value) {
-            result += "&npr=1";
-        }
-    } else {
-        ElMessage({
-            message: '订阅链接不能为空',
-            type: 'error'
-        });
-        linkOutput.value = ""
-        return false;
-    }
-    linkOutput.value = result
-}
-
-const copyForm = () => {
-    navigator.clipboard.writeText(linkOutput.value);
-    ElMessage({
-        message: '复制成功',
-        type: 'success'
+    const form = ref({
+        linkInput: '',
+        linkOutput: '',
+        time: '',
+        standby: '',
+        standby_switch: false,
+        proxy_switch: true,
+        advanced_switch: false
     })
-}
 
+    const keys = Object.keys(form.value)
+
+    onMounted(() => {
+        keys.forEach(key => {
+            if (localStorage.getItem(key)) {
+                if (key.includes('switch') || key === 'advanced_switch') {
+                    form.value[key] = localStorage.getItem(key) === 'true'
+                } else {
+                    form.value[key] = localStorage.getItem(key)
+                }
+            }
+        })
+    })
+
+    const submitForm = () => {
+        if (form.value.linkInput === "") {
+            ElMessage({
+                message: '订阅链接不能为空',
+                type: 'error'
+            })
+            return
+        }
+
+        let result = `${window.location.protocol}//${window.location.host}/sub?url=${encodeURIComponent(form.value.linkInput)}`
+
+        if (form.value.advanced_switch) {
+            if (form.value.time === "0") {
+                form.value.time = ''
+            } else if (form.value.time !== "" && /^[1-9][0-9]*$/.test(form.value.time)) {
+                result += `&interval=${form.value.time}`
+            } else if (form.value.time !== "") {
+                ElMessage({
+                    message: '更新间隔必须为整数',
+                    type: 'error'
+                })
+                form.value.time = ''
+                return
+            }
+            if (form.value.standby_switch && form.value.standby !== "") {
+                result += `&urlstandby=${encodeURIComponent(form.value.standby)}`
+            }
+            if (!form.value.proxy_switch) {
+                result += "&npr=1"
+            }
+        }
+
+        form.value.linkOutput = result
+        copyForm()
+        saveToLocalStorage()
+    }
+
+    const copyForm = () => {
+        navigator.clipboard.writeText(form.value.linkOutput)
+        ElMessage({
+            message: '复制成功',
+            type: 'success'
+        })
+    }
+
+    const installClash = () => {
+        if (form.value.linkOutput === "") {
+            submitForm()
+        }
+        const clashURL = `clash://install-config?url=${encodeURIComponent(form.value.linkOutput)}`
+        window.location.href = clashURL
+        ElMessage({
+            message: '正在启动Clash...',
+            type: 'warning',
+            duration: 5000
+        })
+    }
+
+    const resetForm = () => {
+        keys.forEach(key => {
+            form.value[key] = key === 'proxy_switch' ? true : key.includes('switch') || key === 'advanced_switch' ? false : ''
+            localStorage.removeItem(key)
+        })
+
+        ElMessage({
+            message: '已重置',
+            type: 'success'
+        })
+    }
+
+    const saveToLocalStorage = () => {
+        keys.forEach(key => localStorage.setItem(key, form.value[key]))
+    }
 </script>
 
 <style scoped>
-.box-card {
-    width: 1000px;
-    height: auto;
-    margin: 100px auto auto;
-}
+    .container {
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        padding: 20px;
+    }
 
-.main {
-    margin-top: 60px;
-}
+    .box-card {
+        width: 90%;
+        max-width: 1000px;
+        margin-top: 20px;
+    }
 
-.header {
-    display: block;
-    margin-left: 33px;
-    font-size: 20px;
-    margin-top: 10px;
-    margin-bottom: -30px;
-}
+    .main {
+        margin-top: 20px;
+    }
 
-.footer {
-    text-align: center;
-    margin-top: 20px;
-}
+    .header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: -10px;
+    }
 
-a {
-    color: black;
-	text-decoration: none;
-	position: relative;
-}
-footer a {
-	color: #777777;
-}
+    .footer {
+        text-align: center;
+        margin-top: 20px;
+    }
+
+    .footer div {
+        margin-top: 4px;
+    }
+
+    a {
+        color: black;
+        text-decoration: none;
+    }
+
+    .footer a {
+        color: #777777;
+    }
+
+    .clash-button {
+        background: linear-gradient(to right bottom, #ed87b6, #8f83f7);
+        border: none;
+        color: white;
+    }
+
+    .clash-button:hover {
+        background: linear-gradient(to right bottom, #f4a6c0, #a29bf8);
+    }
+
+    .clash-button:disabled {
+        color: white;
+        background: #cccccc;
+        cursor: not-allowed;
+    }
+
+    @media (max-width: 768px) {
+        .container {
+            padding: 0;
+        }
+
+        .box-card {
+            width: 100%;
+            margin-top: 0;
+        }
+    }
 </style>
