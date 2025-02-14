3f3f5d3805ec471ada256571c83b9cef922cf37a
wouiSB <wouisb@is-sb.com>
2025-02-14 16:48:15 +0800

    change User-Agent

--- api.py
+++ api.py
@@ -96,7 +96,7 @@ async def provider(request: Request):
     headers = {'Content-Type': 'text/yaml;charset=utf-8'}
     url = request.query_params.get("url")
     async with httpx.AsyncClient() as client:
-        resp = await client.get(url, headers={'User-Agent':request.headers['User-Agent']})
+        resp = await client.get(url, headers={'User-Agent': 'v2rayn'})
         if resp.status_code < 200 or resp.status_code >= 400:
             raise HTTPException(status_code=resp.status_code, detail=resp.text)
         result = await parse.parseSubs(resp.text)
@@ -165,13 +165,13 @@ async def sub(request: Request):
         headers = {'Content-Type': 'text/yaml;charset=utf-8'}
         # if there's only one subscription, return userinfo
         if length(url) == 1:
-            resp = await client.head(url[0], headers={'User-Agent':request.headers['User-Agent']})
+            resp = await client.head(url[0], headers={'User-Agent': 'v2rayn'})
             if resp.status_code < 200 or resp.status_code >= 400:
                 raise HTTPException(status_code=resp.status_code, detail=resp.text)
             elif resp.status_code >= 300 and resp.status_code < 400:
                 while resp.status_code >= 300 and resp.status_code < 400:
                     url[0] = resp.headers['Location']
-                    resp = await client.head(url[0], headers={'User-Agent':request.headers['User-Agent']})
+                    resp = await client.head(url[0], headers={'User-Agent': 'v2rayn'})
                     if resp.status_code < 200 or resp.status_code >= 400:
                         raise HTTPException(status_code=resp.status_code, detail=resp.text)
             originalHeaders = resp.headers
@@ -184,7 +184,7 @@ async def sub(request: Request):
         if url is not None:
             for i in range(len(url)):
                 # the test of response
-                respText = (await client.get(url[i], headers={'User-Agent':request.headers['User-Agent']})).text
+                respText = (await client.get(url[i], headers={'User-Agent': 'v2rayn'})).text
                 content.append(await parse.parseSubs(respText))
                 url[i] = "{}provider?{}".format(request.base_url, urlencode({"url": url[i]}))
     if len(content) == 0:
